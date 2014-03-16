e288a1b6038abcbaad74b2b7f53c1114b0a37e7c
mattiadigan <mattiadigangi@gmail.com>
2014-03-16 15:47:18 +0100

    Corretto errore nella creazione del database

--- src/it/bigteam/MainFragmentActivity.java
+++ src/it/bigteam/MainFragmentActivity.java
@@ -1,11 +1,14 @@
 package it.bigteam;
 
+import java.io.IOException;
+
 import it.bigteam.R;
 import it.bigteam.database.DatabaseHelper;
 import it.bigteam.database.UfficioDataSource;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 public class MainFragmentActivity extends FragmentActivity {
 
@@ -14,6 +17,11 @@ public class MainFragmentActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_data_view);
 		DatabaseHelper dbHelper = DatabaseHelper.getInstance(this);
+		try {
+			dbHelper.createDataBase();
+		} catch (IOException e) {
+			Log.d("Database", "Unable to open database");
+		}
 		SQLiteDatabase database = dbHelper.openDataBase();
 		UfficioDataSource dataSource = new UfficioDataSource(database);
 		dbHelper.close();
